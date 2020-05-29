12a990bba013559a11e67712342a6b59c72a2e05
robertjallen <bobbidigi34@gmail.com>
2020-05-28 20:10:05 -0400

    add delete by ID

--- server.js
+++ server.js
@@ -66,26 +66,26 @@ server.post('/api/posts', (req, res) => {
   });
 });
 //------------------------------------------------------------------------
-//                     PUT
+//                     DELETE BY ID
 //------------------------------------------------------------------------
 
-// server.delete('/api/hubs/:id', (req, res) => {
-//   Hubs.remove(req.params.id)
-//   .then(count => {
-//     if (count > 0) {
-//       res.status(200).json({ message: 'The hub has been nuked' });
-//     } else {
-//       res.status(404).json({ message: 'The hub could not be found' });
-//     }
-//   })
-//   .catch(error => {
-//     // log error to database
-//     console.log(error);
-//     res.status(500).json({
-//       message: 'Error removing the hub',
-//     });
-//   });
-// });
+server.delete('/api/posts/:id', (req, res) => {
+  db.remove(req.params.id)
+  .then(count => {
+    if (count > 0) {
+      res.status(200).json({ message: `The post ID: ${req.params.id} has been nuked`, id: `${req.params.id}` });
+    } else {
+      res.status(404).json({ message: 'The post could not be found' });
+    }
+  })
+  .catch(error => {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error removing the post',
+    });
+  });
+});
 
 // server.put('/api/hubs/:id', (req, res) => {
 //   const changes = req.body;
